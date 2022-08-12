48ae0106a3baaef11f1efa6d5db2b1b7d438e4dd
Lvf2i <91951563+Lvf2i@users.noreply.github.com>
2022-08-12 06:18:53 -0700

    Add files via upload

---
+++ R0R77/plugins/ping.py
@@ -0,0 +1,23 @@
+import os
+
+from telethon import Button, events
+
+from R0R77 import *
+
+IMG = os.environ.get(
+    "PING_PIC", "https://telegra.ph/file/8a2b1deadab4f9e15b001.jpg"
+)
+ms = 4
+
+ALIVE = os.environ.get(
+    "ALIVE", "@Hamouddi"
+)
+
+CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
+
+
+@R0R77.on(events.NewMessage(pattern="^/ping"))
+async def _(event):
+    UMM = [[Button.url("القناة", "https://t.me/meeddu")]]
+    await R0R77.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
+ttons=UMM)
